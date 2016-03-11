7cb758955c47df70ffbded48b35a97ddd4e9dc12
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-03-11 14:11:39 -0800

    deploy script tweaks

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+git checkout master
 npm run build
 git branch -D gh-pages
 git checkout --orphan gh-pages
 rm -rf src
 rm -rf templates
-rm build.js && rm README.md && rm package.json
-mv blog/* ./
-rm -rf blog
+rm build.js && rm README.md && rm package.json && rm deploy.sh
+mv build/* ./
+rm -rf build
 git add .
 git commit -m 'Automated deployment.'
+git push origin gh-pages -f
+git checkout master
+echo 'Deployment finished!'
